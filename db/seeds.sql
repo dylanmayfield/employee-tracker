b523928b3a732dd047eb7403e882c4ec974b5a6a
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-11-06 14:59:30 -0600

    added seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ("Enginnering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales");
+
+-- INSERT INTO roles (name, department)
+-- VALUES ("Intro to JavaScript", 1),
+--        ("Data Science", 2),
+--        ("Linear Algebra", 3),
+--        ("History of the Internet", 4),
+--        ("Machine Learning", 4),
+--        ("Game Design", 1 ),
+--        ("Cloud Development", 1);
+       
