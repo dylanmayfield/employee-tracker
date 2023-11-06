d168f847aa6d67c7af01f87d9e84c0442b48842c
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-11-06 14:48:01 -0600

    updated prompt in index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
 inquirer
   .prompt([
     {
       type: 'list',
+      name: 'action',
       message: 'What would you like to do?',
       choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
     }
