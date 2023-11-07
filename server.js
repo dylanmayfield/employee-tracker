dd52b97c57f434cca9c76d600fa20ddf635511b7
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-11-06 18:45:02 -0600

    added response in server,js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const cTable = require('console.table');
+
 
 
 inquirer
@@ -10,9 +12,33 @@ inquirer
             message: 'What would you like to do?',
             choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
         },
-        {
-        
-        }
     ])
-    .then((res) => console.log(res)
-    );
+    .then((res) => {
+        console.log(res)
+        switch (res.action) {
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Quit':
+                quit();
+                break;
+        }
+    });
